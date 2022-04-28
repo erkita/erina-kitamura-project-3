b9b71b9afcee05d6239ffc40064c0bccdb11f6a2
erkita <erkitamura@gmail.com>
2022-04-28 11:35:01 -0700

    added server basic set up

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import postRoutes from "./routes/post.js";
+import userRoutes from "./routes/user.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
+
+const mongoEndpoint =
+  "mongodb+srv://erkita:2062763511@cluster0.vksqs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(mongoEndpoint)
+  .then(() =>
+    app.listen(constants.PORT, () =>
+      console.log(`Server Running on Port ${constants.PORT}`)
+    )
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(mongoEndpoint)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log("this is error"));
