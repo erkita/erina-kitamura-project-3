98bf87b6ef11a0ead086b8dc910ade7546ae805c
erkita <erkitamura@gmail.com>
2022-04-29 14:35:37 -0700

    preparing to deploy to heroku

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
+app.get("/", (req, res) => {
+  res.send("App running succesfully");
+});
+
 const mongoEndpoint =
   "mongodb+srv://erkita:2062763511@cluster0.vksqs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 8000;
